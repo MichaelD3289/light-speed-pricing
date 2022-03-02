ca789c28545b3476a2b407da905e931dce8d23c6
Michael Drummond <michael.drummond328@gmail.com>
2022-03-01 20:10:55 -0700

    added server post endpoints and set ctrl-posts function outline

---
+++ Server/controllerFiles/ctrl-posts.js
@@ -0,0 +1,49 @@
+
+
+module.exports = {
+  addUser: (req, res) => {
+
+  },
+  userSignIn: (req, res) => {
+
+  },
+  addLaser: (req, res) => {
+
+  },
+  addFlatSpeedData: (req, res) => {
+
+  },
+  addRotarySpeedData: (req, res) => {
+
+  },
+  addDefaultData: (req, res) => {
+
+  },
+  addJobDefaults: (req, res) => {
+
+  },
+  addDefaultQtyBreaks: (req, res) => {
+
+  },
+  addDefaultHourlyRates: (req, res) => {
+
+  },
+  addJob: (req, res) => {
+
+  },
+  addFlatJob: (req, res) => {
+
+  },
+  addCustomJobData: (req, res) => {
+
+  },
+  createInvoice: (req, res) => {
+
+  },
+  approveJob: (req, res) => {
+
+  },
+  completeJob: (req, res) => {
+    
+  }
+}
--- Server/server.js
+++ Server/server.js
@@ -3,21 +3,48 @@ const app = express();
 require('dotenv').config();
 
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Client/index.html'));
+})
+
 //Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../Client')));
 
 // DB seed request
 const { seed } = require('../db/dbSeed');
 app.post('/api/seed', seed);
 
+const { addUser, userSignIn, addLaser, addFlatSpeedData, 
+  addRotarySpeedData, addDefaultData, addJob, addFlatJob, 
+  addCustomJobData, addJobDefaults, addDefaultHourlyRates, 
+  addDefaultQtyBreaks, createInvoice, approveJob, completeJob } = require('./controllerFiles/ctrl-posts');
 
-// Post requests
 
-// Get requests
+   // Users
+app.post('/api/users', addUser);
+app.post('/api/user', userSignIn);
 
-// Delete requests
+  // User Defaults
+app.post('api/user/defaults', addDefaultData);
 
-// Put requests
+  //lasers
+app.post('/api/laser', addLaser);
+app.post('/api/laser/flat/speed', addFlatSpeedData);
+app.post('/api/laser/rotary/speed', addRotarySpeedData);
+
+ // Jobs
+app.post('/api/job', addJob);
+app.post('/api/job/flat', addFlatJob);
+app.post('/api/job/custom', addCustomJobData);
+app.post('/api/job/defaults', addJobDefaults);
+app.post('/api/job/defaults/qty', addDefaultQtyBreaks);
+app.post('/api/job/defaults/rates', addDefaultHourlyRates);
+app.post('/api/job/invoice-info', createInvoice);
+app.post('/api/job/approved', approveJob);
+app.post('/api/job/completed', completeJob);
+
+
 
 
 const port = process.env.PORT || process.env.SERVER_PORT;
